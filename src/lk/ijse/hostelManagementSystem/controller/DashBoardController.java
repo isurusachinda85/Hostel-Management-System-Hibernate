79568169c6960806008e624778ecb389a37486b2
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-13 02:09:04 +0530

    update dashboard controller

--- src/lk/ijse/hostelManagementSystem/controller/DashBoardController.java
+++ src/lk/ijse/hostelManagementSystem/controller/DashBoardController.java
@@ -1,6 +1,7 @@
 package lk.ijse.hostelManagementSystem.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
@@ -11,10 +12,36 @@ import java.io.IOException;
 public class DashBoardController {
 
     public AnchorPane dashBoardContext;
+    public AnchorPane mainContext;
 
     public void logOutOnAction(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) dashBoardContext.getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hostelManagementSystem/view/LoginForm.fxml"))));
         stage.centerOnScreen();
+    }
+
+    @FXML
+    void addReservationOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dashBoardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageRoomOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void manageStudentOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void viewReservationOnAction(ActionEvent event) {
+
     }
 }
