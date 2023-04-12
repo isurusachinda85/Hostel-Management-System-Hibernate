2a1abd8cb5e774bf9c1168611d831b317a346dc7
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-13 01:52:26 +0530

    update

--- src/lk/ijse/hostelManagementSystem/controller/DashBoardController.java
+++ src/lk/ijse/hostelManagementSystem/controller/DashBoardController.java
@@ -1,4 +1,20 @@
 package lk.ijse.hostelManagementSystem.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DashBoardController {
+
+    public AnchorPane dashBoardContext;
+
+    public void logOutOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = (Stage) dashBoardContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hostelManagementSystem/view/LoginForm.fxml"))));
+        stage.centerOnScreen();
+    }
 }
