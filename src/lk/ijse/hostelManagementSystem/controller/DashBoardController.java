a33d3c7335fc7dfa49d3a25396dfc81c6fee4da3
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-13 02:49:45 +0530

    update dashboard controller

--- src/lk/ijse/hostelManagementSystem/controller/DashBoardController.java
+++ src/lk/ijse/hostelManagementSystem/controller/DashBoardController.java
@@ -3,6 +3,7 @@ package lk.ijse.hostelManagementSystem.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
@@ -21,27 +22,34 @@ public class DashBoardController {
     }
 
     @FXML
-    void addReservationOnAction(ActionEvent event) {
-
+    void dashBoardOnAction(ActionEvent event) throws IOException {
+        Stage stage = (Stage) mainContext.getScene().getWindow();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hostelManagementSystem/view/DashBoard.fxml"))));
     }
 
     @FXML
-    void dashBoardOnAction(ActionEvent event) {
-
+    void addReservationOnAction(ActionEvent event) throws IOException {
+        setUi("/lk/ijse/hostelManagementSystem/view/addReservationForm");
     }
 
     @FXML
-    void manageRoomOnAction(ActionEvent event) {
-
+    void manageRoomOnAction(ActionEvent event) throws IOException {
+        setUi("/lk/ijse/hostelManagementSystem/view/RoomManageForm");
     }
 
     @FXML
-    void manageStudentOnAction(ActionEvent event) {
-
+    void manageStudentOnAction(ActionEvent event) throws IOException {
+        setUi("/lk/ijse/hostelManagementSystem/view/StudentManageForm");
     }
 
     @FXML
-    void viewReservationOnAction(ActionEvent event) {
+    void viewReservationOnAction(ActionEvent event) throws IOException {
+        setUi("/lk/ijse/hostelManagementSystem/view/viewReservationForm");
+    }
 
+    private void setUi(String ui) throws IOException {
+        Parent load = FXMLLoader.load(getClass().getResource(ui + ".fxml"));
+        mainContext.getChildren().clear();
+        mainContext.getChildren().add(load);
     }
 }
---
+++ src/lk/ijse/hostelManagementSystem/controller/StudentManageFormController.java
@@ -0,0 +1,4 @@
+package lk.ijse.hostelManagementSystem.controller;
+
+public class StudentManageFormController {
+}
