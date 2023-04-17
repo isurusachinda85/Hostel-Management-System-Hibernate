f4f4a8617c7d766e3efaf69d5944760e0eb4ebf6
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-18 02:35:56 +0530

    update

--- src/lk/ijse/hostelManagementSystem/controller/StudentManageFormController.java
+++ src/lk/ijse/hostelManagementSystem/controller/StudentManageFormController.java
@@ -1,4 +1,96 @@
 package lk.ijse.hostelManagementSystem.controller;
 
-public class StudentManageFormController {
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentManageFormController implements Initializable {
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtAddress;
+
+    @FXML
+    private JFXTextField txtContact;
+
+    @FXML
+    private JFXDatePicker txtDob;
+
+    @FXML
+    private JFXComboBox<String> cmbGender;
+
+    @FXML
+    private TableView<?> tblStudent;
+
+    @FXML
+    private TableColumn<?, ?> colId;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colContact;
+
+    @FXML
+    private TableColumn<?, ?> colDob;
+
+    @FXML
+    private TableColumn<?, ?> colGender;
+
+    @FXML
+    private TableColumn<?, ?> colRegisterDate;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setCmbGender();
+    }
+    @FXML
+    void clearTextOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+    public void setCmbGender(){
+        ObservableList<String>list = FXCollections.observableArrayList();
+        list.addAll("Male","Female","Other");
+        cmbGender.setItems(list);
+    }
+
 }
