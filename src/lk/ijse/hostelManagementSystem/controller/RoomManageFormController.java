daa965e2814718ba7d04e75131e30e3dcdb17660
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-18 17:49:21 +0530

    update

--- src/lk/ijse/hostelManagementSystem/controller/RoomManageFormController.java
+++ src/lk/ijse/hostelManagementSystem/controller/RoomManageFormController.java
@@ -1,4 +1,81 @@
 package lk.ijse.hostelManagementSystem.controller;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class RoomManageFormController {
+
+    @FXML
+    private JFXTextField txtId;
+
+    @FXML
+    private JFXComboBox<?> cmbRoomType;
+
+    @FXML
+    private JFXTextField txtMonthlyRent;
+
+    @FXML
+    private JFXTextField txtqty;
+
+    @FXML
+    private TableView<?> tblRoom;
+
+    @FXML
+    private TableColumn<?, ?> colRoomId;
+
+    @FXML
+    private TableColumn<?, ?> colType;
+
+    @FXML
+    private TableColumn<?, ?> colMonthlyRent;
+
+    @FXML
+    private TableColumn<?, ?> colRoomQty;
+
+    @FXML
+    private TableColumn<?, ?> colAvailableQty;
+
+    @FXML
+    private TableColumn<?, ?> colAddDate;
+
+    @FXML
+    private JFXDatePicker txtAddDate;
+
+    @FXML
+    private Label lblDate;
+
+    @FXML
+    private Label lblTime;
+
+    @FXML
+    void clearOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void updateOnAction(ActionEvent event) {
+
+    }
+
 }
