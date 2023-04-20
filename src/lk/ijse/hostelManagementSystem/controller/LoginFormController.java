3de71864404b53132f906b0bb4470676506e2e06
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-21 02:27:49 +0530

    update

--- src/lk/ijse/hostelManagementSystem/controller/LoginFormController.java
+++ src/lk/ijse/hostelManagementSystem/controller/LoginFormController.java
@@ -3,21 +3,22 @@ package lk.ijse.hostelManagementSystem.controller;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import com.jfoenix.controls.JFXToggleNode;
-import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Parent;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import lk.ijse.hostelManagementSystem.entity.User;
 import lk.ijse.hostelManagementSystem.util.FactoryConfiguration;
+import org.controlsfx.control.Notifications;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 
@@ -52,15 +53,16 @@ public class LoginFormController {
         Session session = FactoryConfiguration.getInstance().getSession();
         String hql = "From User where userName =:user_name and password=:password";
         Query query = session.createQuery(hql);
-        query.setParameter("user_name",userName);
-        query.setParameter("password",password);
+        query.setParameter("user_name", userName);
+        query.setParameter("password", password);
         User user = (User) query.uniqueResult();
         if (user != null) {
             Stage stage = (Stage) loginContext.getScene().getWindow();
             stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hostelManagementSystem/view/DashBoard.fxml"))));
             stage.centerOnScreen();
-        }else {
-            System.out.println("error");
+            successNotification();
+        } else {
+            failNotification();
         }
         session.close();
 
@@ -71,4 +73,26 @@ public class LoginFormController {
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/lk/ijse/hostelManagementSystem/view/SignUpForm.fxml"))));
         stage.centerOnScreen();
     }
+
+    public void successNotification() {
+        Notifications notificationBuilder = Notifications.create()
+                .title("Success !")
+                .text("You have Successfully Login")
+                .graphic(new ImageView(new Image("/lk/ijse/hostelManagementSystem/view/assest/Done.png")))
+                .hideAfter(Duration.seconds(8))
+                .position(Pos.BOTTOM_RIGHT);
+        notificationBuilder.darkStyle();
+        notificationBuilder.show();
+    }
+    public void failNotification(){
+        Notifications notificationBuilder = Notifications.create()
+                .title("UnSuccessful !")
+                .text("Please enter Correct User Name or Password.Try Again.!")
+                .graphic(new ImageView(new Image("/lk/ijse/hostelManagementSystem/view/assest/Wrong.png")))
+                .hideAfter(Duration.seconds(8))
+                .position(Pos.BOTTOM_RIGHT);
+        notificationBuilder.darkStyle();
+        notificationBuilder.show();
+    }
+
 }
