2b0d923dd34d4fd988eaf78c296a422520c9f9cd
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-22 01:46:02 +0530

    update

--- src/lk/ijse/hostelManagementSystem/controller/ManageUserController.java
+++ src/lk/ijse/hostelManagementSystem/controller/ManageUserController.java
@@ -4,37 +4,72 @@ import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import lk.ijse.hostelManagementSystem.entity.User;
+import lk.ijse.hostelManagementSystem.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.io.IOException;
 
 public class ManageUserController {
 
     @FXML
-    private JFXTextField currentUserName;
+    private JFXTextField txtCurrentUserName;
 
     @FXML
-    private JFXPasswordField currentPassword;
+    private JFXPasswordField txtCurrentPassword;
 
     @FXML
-    private JFXPasswordField confirmPassword;
+    private JFXPasswordField txtConfirmPassword;
 
     @FXML
-    private JFXTextField newUserName;
+    private JFXTextField txtNewUserName;
 
     @FXML
-    private JFXPasswordField newPassword;
+    private JFXPasswordField txtNewPassword;
+
+    @FXML
+    private JFXTextField txtUserId;
+
+    @FXML
+    void updateOnAction(ActionEvent event) throws IOException {
+        int id = Integer.parseInt(txtUserId.getText());
+        String newUserName = txtNewUserName.getText();
+        String newPassword = txtNewPassword.getText();
+        String confirmPassword = txtConfirmPassword.getText();
+
+        User user = new User(id, newUserName, newPassword, confirmPassword);
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(user);
+        transaction.commit();
+        session.close();
+    }
+
+    @FXML
+    void userOnAction(ActionEvent event) throws IOException {
+        int id = Integer.parseInt(txtUserId.getText());
+
+        Session session = FactoryConfiguration.getInstance().getSession();
+        Transaction transaction = session.beginTransaction();
+        User user = session.get(User.class, id);
+        if (user != null) {
+            txtCurrentUserName.setText(user.getUserName());
+            txtCurrentPassword.setText(user.getPassword());
+        }
+        transaction.commit();
+        session.close();
+    }
 
     @FXML
     void cancelOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void updateOnAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void userNameOnAction(ActionEvent event) {
-
+        txtUserId.clear();
+        txtCurrentUserName.clear();
+        txtCurrentPassword.clear();
+        txtNewUserName.clear();
+        txtNewPassword.clear();
+        txtConfirmPassword.clear();
     }
 
 }
