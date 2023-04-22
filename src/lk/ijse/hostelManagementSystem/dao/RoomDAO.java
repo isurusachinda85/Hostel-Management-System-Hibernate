ac78ca5f238a03f434f7319053b7b1da66b7e6e7
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-22 07:14:55 +0530

    update roomDAO

--- src/lk/ijse/hostelManagementSystem/dao/RoomDAO.java
+++ src/lk/ijse/hostelManagementSystem/dao/RoomDAO.java
@@ -1,4 +1,22 @@
 package lk.ijse.hostelManagementSystem.dao;
 
+import lk.ijse.hostelManagementSystem.entity.Room;
+import lk.ijse.hostelManagementSystem.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface RoomDAO {
+    boolean saveRoom(Room room) throws IOException;
+
+    Room searchRoom(String id) throws IOException;
+
+    boolean updateRoom(Room room) throws IOException;
+
+    boolean deleteRoom(String id) throws IOException;
+
+    List<Room> getAllRoom() throws IOException;
 }
