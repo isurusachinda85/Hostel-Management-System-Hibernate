35c914eae5e2426d3322fa370fdb86b6b85082ef
isurusachinda85 <isurusachintha8@gmail.com>
2023-04-22 07:13:31 +0530

    update studentDAO

--- src/lk/ijse/hostelManagementSystem/dao/StudentDAO.java
+++ src/lk/ijse/hostelManagementSystem/dao/StudentDAO.java
@@ -1,4 +1,22 @@
 package lk.ijse.hostelManagementSystem.dao;
 
+import lk.ijse.hostelManagementSystem.entity.Student;
+import lk.ijse.hostelManagementSystem.util.FactoryConfiguration;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.io.IOException;
+import java.util.List;
+
 public interface StudentDAO {
+    boolean saveStudent(Student student) throws IOException;
+
+    Student searchStudent(String id) throws IOException;
+
+    boolean updateStudent(Student student) throws IOException;
+
+    boolean deleteStudent(String id) throws IOException;
+
+    List<Student> getAllStudent() throws IOException;
 }
